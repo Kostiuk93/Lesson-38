c504fe97c57f56b7282cf65c36a48ca2cd02fc79
Kostiuk93 <kostiuk93@gmail.com>
2021-11-03 04:56:31 +0300

    Lesson 43 Создаем модальное окно

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+
+//Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
             tabsContent = document.querySelectorAll('.tabcontent'),
             tabsParent = document.querySelector('.tabheader__items');
@@ -32,6 +34,111 @@ window.addEventListener('DOMContentLoaded', () => {
                     showTabContent(i); 
                 }  
             });
+        }
+    });
+
+//Timer
+
+    const deadline = '2021-11-05'; //Задаем начальную точку отсчета
+
+    function getTimeRemaining(endtime) {
+        const   t = Date.parse(endtime) - Date.parse(new Date()), //разница между конечной датой и нынешней датой в миллисекундах
+                days = Math.floor(t / (1000 * 60 * 60 *24)),  //Math.floor() округление до ближайшего целого целого
+                hours = Math.floor((t / (1000 * 60 * 60) & 24)), //вычисление остатка оставшихся часов
+                minutes = Math.floor((t / 1000 / 60) % 60),     //вычисление остатка оставшихся минут
+                seconds = Math.floor((t / 1000 ) %60);          //вычисление остатка оставшихся секунд
+
+        // Создание объекта
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+    // функция для того чтобы перед числом меньше 10 стоял ноль
+    function getZero (num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+        updateClock(); // убирает мигание верстки, то есть задержку запуска функции updateClock() на странице
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('.timer', deadline);
+
+//Create Modal Window
+
+    const   modWin = document.querySelector('.modal'),
+            modBtn = document.querySelectorAll('[data-modal]'),
+            modClose = document.querySelector('[data-close]');
+
+    function modalOpen() {
+    // modWin.style.display = 'flex';       // 1 способ смены стилей Css
+
+        modWin.classList.add('show');       // 2 способ смены стилей
+        modWin.classList.remove('hide');    // 2 способ смены стилей
+
+        // modWin.classList.toggle('show');       // 3 способ смены стилей
+        document.body.style.overflow = 'hidden'; //отключение прокрутки экрана при вызове модального окна
+
+    }
+    function modalClose() {
+        // modWin.style.display = 'none';    // 1 способ смены стилей Css
+
+        modWin.classList.add('hide');        // 2 способ смены стилей
+        modWin.classList.remove('show');     // 2 способ смены стилей
+
+        // modWin.classList.toggle('show');       // 3 способ смены стилей
+        document.body.style.overflow = '';    //возврат прокрутки экрана 
+    }
+
+    // Открыть модальные окна
+    modBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modalOpen();
+        });
+    });
+    
+    // Закрыть модальное окно
+    modClose.addEventListener('click', modalClose); 
+
+    // Закрытие модального окна кликом на пустое пространство вокруг модального окна
+    modWin.addEventListener('click', (e) => {
+        if (e.target === modWin) {
+            modalClose();
+        }
+    });
+
+    // Закрыть модальное окно при нажатии клавиши на клавиатуре 
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modWin.classList.contains('show')) {
+            modalClose();
+        } if (e.code === "KeyM" ) {
+            modalOpen();
         }
     });
 });
